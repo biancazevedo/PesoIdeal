660db89e7e76c38f11b6364fdf92cf8cf08f7662
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-05-20 08:33:04 -0300

    acabei

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double height, pesoIdeal;
+string sexo;
+
+Console.Write("Digite sua height em m..........: ");
+height = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Sexo [M]an / [W]oman...: ");
+sexo = Console.ReadLine()!.ToUpper().Trim();
+
+if (sexo != "M" && sexo != "W")
+{
+    Console.WriteLine("Não possuo fórmula para cálculo.");
+}
+else
+{
+    if (sexo == "M")
+    {
+        pesoIdeal = height * 72.7 - 58.0;
+    }
+    else
+    {
+        pesoIdeal = height * 62.1 - 44.7;
+    }
+
+    Console.WriteLine($"Seu peso ideal é {pesoIdeal:N1}kg.");
+}
